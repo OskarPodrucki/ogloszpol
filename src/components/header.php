78ccbf898df4c7a81d577c61024d23feb65abf9b
OskarPodrucki <oskar.podrucki1@gmail.com>
2024-03-24 19:23:00 +0100

    ...

---
+++ src/components/header.php
@@ -0,0 +1,17 @@
+<div id="header">
+
+    <div id="centerLogo">
+        <a href="index.php"><img id="logo" src="../img/pagelook/ogloszpo!.png" alt="logo"></a>
+    </div>
+
+
+    <div id="newAnnouncement">
+        <button>Nowe ogłoszenie</button>
+    </div>
+
+    <div id="auth">
+        <button>Twoje konto</button>
+    </div>
+
+
+</div>
