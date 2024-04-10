9e1dd25f742219e2e63d7bcbcca5ed0b69b91462
Oskar <118886547+Oskar090@users.noreply.github.com>
2024-04-10 15:33:38 +0200

    ...

--- db/ogloszpol.sql
+++ db/ogloszpol.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 09 Kwi 2024, 14:46
+-- Czas generowania: 10 Kwi 2024, 15:32
 -- Wersja serwera: 10.4.27-MariaDB
 -- Wersja PHP: 8.2.0
 
@@ -38,9 +38,23 @@ CREATE TABLE `kategorie` (
 --
 
 INSERT INTO `kategorie` (`id`, `nazwa`, `opis`) VALUES
-(4, 'rolnictwo', 'rolnictwo i te sprawy\r\n'),
-(5, 'motoryzacja', 'motoryzacja'),
-(6, 'elektronika', 'elektronika');
+(4, 'Rolnictwo', 'rolnictwo i te sprawy\r\n'),
+(5, 'Motoryzacja', 'motoryzacja'),
+(6, 'Elektronika', 'elektronika'),
+(7, 'Nieruchomośći', 'Domy, działki itd.'),
+(8, 'Antyki i kolekcje', 'Dla kolekcjonerów i fanatyków'),
+(9, 'Praca', 'Praca, praca, praca...'),
+(10, 'Książki', 'Różnego rodzaju książki'),
+(11, 'Biżuteria', 'Akcesoria i ozdoby personalne'),
+(12, 'Sport', 'Sprzęt sportowy i ubiór'),
+(13, 'Ogród', 'Do ogrodu'),
+(14, 'Żywność', 'Produkty spożywcze i artykuły spożywcze'),
+(15, 'Film i muzyka', 'Filmy, muzyka i multimedia'),
+(16, 'Zdrowie i uroda', 'Produkty kosmetyczne i artykuły higieniczne'),
+(17, 'Gry i zabawki', 'Gry planszowe, konsolowe i zabawki'),
+(18, 'Gruz', 'Poprostu gruz'),
+(19, 'Artykuły dla zwierząt', 'Produkty dla zwierząt domowych'),
+(20, 'Meble', 'Meble itd.');
 
 -- --------------------------------------------------------
 
@@ -79,17 +93,18 @@ CREATE TABLE `ogloszenia` (
 --
 
 INSERT INTO `ogloszenia` (`id`, `użytkownikId`, `zdjecie_url`, `tytul`, `opis`, `kategoria`, `cena`, `lokalizacja`, `kontakt_telefoniczny`, `data_dodania`) VALUES
-(4, 1, '1', 'ciągnik john deere', 'bardzo dobry ciągnij john deere', 1, '25000.00', 'warszawa', '123 123 123', '2024-03-27 18:51:07'),
-(5, 10, '2', 'test', 'test', 4, '12300.00', 'test', '312 213 321', '0000-00-00 00:00:00'),
-(6, 10, '2', 'test2', 'test2', 4, '12312.00', 'test2', '123 532 432', '2024-04-03 11:33:42'),
-(7, 10, '2', 'test2', 'test2', 4, '12312.00', 'test2', '123 532 432', '2024-04-03 11:34:08'),
-(8, 10, '2', 'test2', 'test2', 4, '12312.00', 'test2', '123 532 432', '2024-04-03 11:34:12'),
-(9, 10, '2', 'test2', 'test2', 4, '12312.00', 'test2', '123 532 432', '2024-04-03 11:34:15'),
-(10, 10, '2', 'test2', 'test2', 4, '12312.00', 'test2', '123 532 432', '2024-04-03 11:34:17'),
-(11, 10, '2', 'test2', 'test2', 4, '12312.00', 'test2', '123 532 432', '2024-04-03 11:34:22'),
-(12, 10, '1', 'niewolnicy', 'tani niewolnicy na sprzedaz', 4, '5.00', 'niger', '213742069', '2024-04-03 12:58:02'),
+(4, 1, '1', 'ciągnik john deere', 'bardzo dobry ciągnij john deere', 4, '25000.00', 'warszawa', '123 123 123', '2024-03-27 18:51:07'),
+(12, 10, '1', 'kombajn', 'fajny kombaj na sprzedarz', 4, '5.00', 'lublin', '213742069', '2024-04-03 12:58:02'),
 (13, 10, '3', 'komputrak', 'superkomputraka', 6, '150.00', 'warszawa', '312 512 321', '2024-04-09 11:45:34'),
-(14, 10, '2', 'motor', 'motor', 5, '123.00', 'warszawa', '312 123 523', '2024-04-09 11:55:34');
+(14, 10, '2', 'motor', 'motor', 5, '123.00', 'warszawa', '312 123 523', '2024-04-09 11:55:34'),
+(15, 10, '12', 'laptopik', 'fajny laptop do roboty i do pogrania w słabsze giereczki', 6, '2500.00', 'mistów', '213 511 213', '2024-04-10 06:49:14'),
+(16, 10, '12', 'laptopik', 'fajny laptop do roboty i do pogrania w słabsze giereczki', 6, '2500.00', 'mistów', '213 511 213', '2024-04-10 06:51:11'),
+(17, 10, '12', 'opryskiwacz', 'bardzo fajny osprzęt działa i jest super', 4, '1234.00', 'anielinek', '521 351 531', '2024-04-10 06:52:24'),
+(18, 10, '1', 'rtx 4090', 'fajna karta graficzna', 6, '3500.00', 'warszawa', '123 534 523', '2024-04-10 06:53:14'),
+(19, 10, '11', 'gruz (Volkswagen golf)', 'fajny gruzik :D', 5, '3212.00', 'warszawa', '431 342 254', '2024-04-10 06:54:19'),
+(20, 10, '8', 'ursus c360', 'fajny ciągnik', 4, '2312.00', 'katowice', '235 523 123', '2024-04-10 06:55:45'),
+(21, 10, '8', 'ursus c360', 'fajny ciągnik', 4, '2312.00', 'katowice', '235 523 123', '2024-04-10 06:56:36'),
+(22, 10, '8', 'zegarek taki fany', 'zegareczek', 8, '123.00', 'katowice', ' 412 123 342', '2024-04-10 13:05:16');
 
 -- --------------------------------------------------------
 
@@ -99,9 +114,17 @@ INSERT INTO `ogloszenia` (`id`, `użytkownikId`, `zdjecie_url`, `tytul`, `opis`,
 
 CREATE TABLE `polubienia` (
   `id_polubienia` int(11) NOT NULL,
+  `id_ogloszenia` int(11) NOT NULL,
   `id_uzytkownika` int(11) NOT NULL,
   `kiedy_polubiono` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Zrzut danych tabeli `polubienia`
+--
+
+INSERT INTO `polubienia` (`id_polubienia`, `id_ogloszenia`, `id_uzytkownika`, `kiedy_polubiono`) VALUES
+(1, 5, 1, '2024-04-09');
 
 -- --------------------------------------------------------
 
@@ -160,8 +183,15 @@ CREATE TABLE `zamówienia` (
   `id_zamowienia` int(11) NOT NULL,
   `id_sprzedawcy` int(11) NOT NULL,
   `id_kupującego` int(11) NOT NULL,
-  `kiedy_zamowiono` int(11) NOT NULL
+  `kiedy_zamowiono` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Zrzut danych tabeli `zamówienia`
+--
+
+INSERT INTO `zamówienia` (`id_zamowienia`, `id_sprzedawcy`, `id_kupującego`, `kiedy_zamowiono`) VALUES
+(0, 10, 1, '2024-04-08');
 
 -- --------------------------------------------------------
 
@@ -171,8 +201,32 @@ CREATE TABLE `zamówienia` (
 
 CREATE TABLE `zdjecia_kategorie` (
   `id` int(11) NOT NULL,
-  `url` varchar(256) NOT NULL
+  `id_kategorii` int(11) NOT NULL,
+  `url` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Zrzut danych tabeli `zdjecia_kategorie`
+--
+
+INSERT INTO `zdjecia_kategorie` (`id`, `id_kategorii`, `url`) VALUES
+(1, 4, '../../img/categoryimg/category4.png'),
+(2, 5, '../../img/categoryimg/category5.png'),
+(3, 6, '../../img/categoryimg/category6.png'),
+(4, 7, '../../img/categoryimg/category7.png'),
+(5, 8, '../../img/categoryimg/category8.png'),
+(6, 9, '../../img/categoryimg/category9.png'),
+(7, 10, '../../img/categoryimg/category10.png'),
+(8, 11, '../../img/categoryimg/category11.png'),
+(9, 12, '../../img/categoryimg/category12.png'),
+(10, 13, '../../img/categoryimg/category13.png'),
+(11, 14, '../../img/categoryimg/category14.png'),
+(12, 15, '../../img/categoryimg/category15.png'),
+(13, 16, '../../img/categoryimg/category16.png'),
+(14, 17, '../../img/categoryimg/category17.png'),
+(15, 18, '../../img/categoryimg/category18.png'),
+(16, 19, '../../img/categoryimg/category19.png'),
+(17, 20, '../../img/categoryimg/category20.png');
 
 -- --------------------------------------------------------
 
@@ -193,8 +247,117 @@ CREATE TABLE `zdjecia_ogloszenia` (
 
 CREATE TABLE `zdjecia_uzytkownicy` (
   `id` int(11) NOT NULL,
-  `url` int(11) NOT NULL
+  `id_kategorii` int(11) NOT NULL,
+  `url` varchar(500) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Zrzut danych tabeli `zdjecia_uzytkownicy`
+--
+
+INSERT INTO `zdjecia_uzytkownicy` (`id`, `id_kategorii`, `url`) VALUES
+(1, 4, '0'),
+(2, 5, '0'),
+(3, 6, '0'),
+(4, 7, '0'),
+(5, 8, '0'),
+(6, 9, '0'),
+(7, 10, '0'),
+(8, 11, '0'),
+(9, 12, '0'),
+(10, 13, '0'),
+(11, 14, '0'),
+(12, 15, '0'),
+(13, 16, '0'),
+(14, 17, '0'),
+(15, 18, '0'),
+(16, 19, '0'),
+(17, 20, '0'),
+(1, 4, '0'),
+(2, 5, '0'),
+(3, 6, '0'),
+(4, 7, '0'),
+(5, 8, '0'),
+(6, 9, '0'),
+(7, 10, '0'),
+(8, 11, '0'),
+(9, 12, '0'),
+(10, 13, '0'),
+(11, 14, '0'),
+(12, 15, '0'),
+(13, 16, '0'),
+(14, 17, '0'),
+(15, 18, '0'),
+(16, 19, '0'),
+(17, 20, '0'),
+(0, 4, '0'),
+(0, 5, '0'),
+(0, 6, '0'),
+(0, 7, '0'),
+(0, 8, '0'),
+(0, 9, '0'),
+(0, 10, '0'),
+(0, 11, '0'),
+(0, 12, '0'),
+(0, 13, '0'),
+(0, 14, '0'),
+(0, 15, '0'),
+(0, 16, '0'),
+(0, 17, '0'),
+(0, 18, '0'),
+(0, 19, '0'),
+(0, 20, '0'),
+(0, 4, '0'),
+(0, 5, '0'),
+(0, 6, '0'),
+(0, 7, '0'),
+(0, 8, '0'),
+(0, 9, '0'),
+(0, 10, '0'),
+(0, 11, '0'),
+(0, 12, '0'),
+(0, 13, '0'),
+(0, 14, '0'),
+(0, 15, '0'),
+(0, 16, '0'),
+(0, 17, '0'),
+(0, 18, '0'),
+(0, 19, '0'),
+(0, 20, '0'),
+(0, 4, '0'),
+(0, 5, '0'),
+(0, 6, '0'),
+(0, 7, '0'),
+(0, 8, '0'),
+(0, 9, '0'),
+(0, 10, '0'),
+(0, 11, '0'),
+(0, 12, '0'),
+(0, 13, '0'),
+(0, 14, '0'),
+(0, 15, '0'),
+(0, 16, '0'),
+(0, 17, '0'),
+(0, 18, '0'),
+(0, 19, '0'),
+(0, 20, '0'),
+(0, 4, '../../img/categoryimg/category4.png'),
+(0, 5, '../../img/categoryimg/category5.png'),
+(0, 6, '../../img/categoryimg/category6.png'),
+(0, 7, '../../img/categoryimg/category7.png'),
+(0, 8, '../../img/categoryimg/category8.png'),
+(0, 9, '../../img/categoryimg/category9.png'),
+(0, 10, '../../img/categoryimg/category10.png'),
+(0, 11, '../../img/categoryimg/category11.png'),
+(0, 12, '../../img/categoryimg/category12.png'),
+(0, 13, '../../img/categoryimg/category13.png'),
+(0, 14, '../../img/categoryimg/category14.png'),
+(0, 15, '../../img/categoryimg/category15.png'),
+(0, 16, '../../img/categoryimg/category16.png'),
+(0, 17, '../../img/categoryimg/category17.png'),
+(0, 18, '../../img/categoryimg/category18.png'),
+(0, 19, '../../img/categoryimg/category19.png'),
+(0, 20, '../../img/categoryimg/category20.png');
 
 -- --------------------------------------------------------
 
@@ -265,7 +428,7 @@ ALTER TABLE `zdjecia_ogloszenia`
 -- AUTO_INCREMENT dla tabeli `kategorie`
 --
 ALTER TABLE `kategorie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT dla tabeli `logowania`
@@ -277,7 +440,7 @@ ALTER TABLE `logowania`
 -- AUTO_INCREMENT dla tabeli `ogloszenia`
 --
 ALTER TABLE `ogloszenia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT dla tabeli `uprawnienia`
@@ -295,7 +458,7 @@ ALTER TABLE `uzytkownicy`
 -- AUTO_INCREMENT dla tabeli `zdjecia_kategorie`
 --
 ALTER TABLE `zdjecia_kategorie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT dla tabeli `zdjecia_ogloszenia`
